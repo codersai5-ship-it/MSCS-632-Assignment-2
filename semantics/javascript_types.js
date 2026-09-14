55160cd69b02207f22a953b2bdcc5504e7edaf0c
codersai5-ship-it <codersai5@gmail.com>
2026-09-13 23:01:49 -0500

    Add JavaScript type system example

---
+++ semantics/javascript_types.js
@@ -0,0 +1,12 @@
+// JavaScript: Type System Example
+let value = 10;
+console.log("Value:", value);
+console.log("Type:", typeof value);
+
+value = "Hello";
+console.log("Value:", value);
+console.log("Type:", typeof value);
+
+value = 10.5;
+console.log("Value:", value);
+console.log("Type:", typeof value);
