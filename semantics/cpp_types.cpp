7aba06c8f19a4e1d89191de88c1386f99104c8aa
codersai5-ship-it <codersai5@gmail.com>
2026-09-13 23:01:54 -0500

    Add C++ type system example

---
+++ semantics/cpp_types.cpp
@@ -0,0 +1,16 @@
+// C++: Type System Example
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int wholeNumber = 10;
+    string text = "Hello";
+    double decimalNumber = 10.5;
+
+    cout << "Integer value: " << wholeNumber << endl;
+    cout << "String value: " << text << endl;
+    cout << "Double value: " << decimalNumber << endl;
+
+    return 0;
+}
