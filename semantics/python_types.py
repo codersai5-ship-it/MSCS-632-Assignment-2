96db6e6704260673ed78c16f3a4a0783db52b599
codersai5-ship-it <codersai5@gmail.com>
2026-09-13 23:01:45 -0500

    Add Python type system example

---
+++ semantics/python_types.py
@@ -0,0 +1,12 @@
+# Python: Type System Example
+value = 10
+print("Value:", value)
+print("Type:", type(value))
+
+value = "Hello"
+print("Value:", value)
+print("Type:", type(value))
+
+value = 10.5
+print("Value:", value)
+print("Type:", type(value))
